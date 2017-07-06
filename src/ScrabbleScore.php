6aa8cc017a1bda247480d9e9739edb0be11f5f72
Dylan Lewis and Nathan Stewart <nathan@>
2017-07-06 10:13:23 -0700

    modify function to account for multi-character words - pass test

--- src/ScrabbleScore.php
+++ src/ScrabbleScore.php
@@ -5,26 +5,34 @@
 
         function scrabbleScoreKeeper($input)
         {
+
             $input = strtolower($input);
 
             if (preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $input) || (ctype_digit($input))) {
-                $score = "Please enter letters only!";
-            } elseif (($input == "a") || ($input == "e") || ($input == "i") || ($input == "o") || ($input == "u") || ($input == "l") || ($input == "n") || ($input == "r") || ($input == "s") || ($input == "t")) {
-                $score = "1";
-            } elseif (($input == "d") || ($input == "g")) {
-                $score = "2";
-            } elseif (($input == "b") || ($input == "c") || ($input == "m") || ($input == "p")) {
-                $score = "3";
-            } elseif (($input == "f") || ($input == "h") || ($input == "v") || ($input == "w") || ($input == "y")) {
-                $score = "4";
-            } elseif ($input == "k") {
-                $score = "5";
-            } elseif (($input == "j") || ($input == "x")) {
-                $score = "8";
-            } elseif (($input == "q") || ($input == "z")) {
-                $score = "10";
+              $this->score = "Please enter letters only!";
+            } else {
+
+            $input = str_split($input);
+
+            foreach($input as $letter) {
+              if (($letter == "a") || ($letter == "e") || ($letter == "i") || ($letter == "o") || ($letter == "u") || ($letter == "l") || ($letter == "n") || ($letter == "r") || ($letter == "s") || ($letter == "t")) {
+                $this->score += 1;
+              } elseif (($letter == "d") || ($letter == "g")) {
+                $this->score += 2;
+              } elseif (($letter == "b") || ($letter == "c") || ($letter == "m") || ($letter == "p")) {
+                $this->score += 3;
+              } elseif (($letter == "f") || ($letter == "h") || ($letter == "v") || ($letter == "w") || ($letter == "y")) {
+                $this->score += 4;
+              } elseif ($letter == "k") {
+                $this->score += 5;
+              } elseif (($letter == "j") || ($letter == "x")) {
+                $this->score += 8;
+              } elseif (($letter == "q") || ($letter == "z")) {
+                $this->score += 10;
+              }
             }
-            return $score;
+          }
+          return strval($this->score);
         }
     }
 ?>
